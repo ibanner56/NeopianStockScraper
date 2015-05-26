b081d355257bf7f7ef6a89b4280ee5c1f7d964b6
Isaac Banner <ibanner56@gmail.com>
2015-05-25 22:38:42 -0400

    Can now grab and print out all the stocks and their current price. Moving forward I want to also scrape the user's portfolio and send out alerts when a stock in their portfolio is selling above a preset threshold.

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,10 @@
 import mechanize
 import cookielib
-from BeautifulSoup import BeautifulSoup
-import html2text
+from bs4 import BeautifulSoup
+import re
+
+def strip(s):
+	return re.sub(r'\W+', '', s)
 
 # Browser
 br = mechanize.Browser()
@@ -12,7 +15,6 @@ br.set_cookiejar(cj)
 
 # Browser options
 br.set_handle_equiv(True)
-br.set_handle_gzip(True)
 br.set_handle_redirect(True)
 br.set_handle_referer(True)
 br.set_handle_robots(False)
@@ -21,27 +23,51 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('User-agent', 'Chrome')]
 
 # The site we will navigate into, handling it's session
-br.open('http://neopets.com/login.phtml')
+br.open('http://neopets.com/login/')
 
 # View available forms
 for f in br.forms():
 	print(f)
 
 # Select the second (index one) form (the first form is a search query box)
-br.select_form(nr=1)
+br.select_form(nr=0)
 
 # User credentials
+# TODO: MAKE SURE YOU FILL THESE IN YOU DINGUS
 br.form['username'] = ''
 br.form['password'] = ''
-br.form['destination'] = 'stockmarket.phtml?type=list&full=true'
 
 # Login
 br.submit()
 
-print(br.open('https://github.com/settings/emails').read())
+# Grab the stock market page and, because the people that make neopets don't
+# use classes or id's in their CSS (WTF), get all the tables in the page.
+# The only table that has "align: center" is the stock list, so we grab that one.
+response = br.open("http://www.neopets.com/stockmarket.phtml?type=list&full=true")
+soup = BeautifulSoup(response)
+tables = soup.find_all("table")
 
+for t in tables:
+	if t.get("align") == "center":
+		stock_table = t.prettify()
+		stocks = stock_table.split("<tr>");
+		for stock_entry in stocks[1:]:
+			stock = stock_entry.split("<td")
+			
+			# Ticker Symbol
+			ticker = stock[2]
+			ticker = ticker[ticker.find("<b>") + 3 : ticker.find("</b>")]
+			ticker = strip(ticker)
 
-# Login
-br.submit()
+			# Name
+			name = stock[3]
+			name = name[name.find(">") + 1 : name.find("<")]
+			name = strip(name)
 
-print(br.open('https://github.com/settings/emails').read())
+			# Sales Volume
+			price = stock[5]
+			price = price[price.find("<b>") + 3 : price.find("</b>")]
+			price = strip(price)
+
+			print(ticker + " - " + name)
+			print("Current Stock Price: " + price)
