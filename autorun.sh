4c7cbc9f6cff18b0836114754633c89719f681f0
Isaac Banner <ibanner56@gmail.com>
2015-06-12 08:43:34 -0600

    Update autorun.sh
    Since it's being run by cron it's being run from the root directory. Needed to include path to .py file.

--- autorun.sh
+++ autorun.sh
@@ -2,7 +2,7 @@ TIME=`date +%T | cut -d':' -f-2`
 HOUR=`echo $TIME | cut -d':' -f1`
 MINUTE=`echo $TIME | cut -d':' -f2`
 if [ "$HOUR" = "08" -a "$MINUTE" = "30" ]; then
-	python Scraper.py -d
+	python /<path-to-repo>/Scraper.py -d
 else
-	python Scraper.py
+	python /<path-to-repo>/Scraper.py
 fi 
